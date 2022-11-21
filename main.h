cd3c41352662b248897e4730acb6754cd8a78213
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-21 10:11:04 -0800

    Update main.h

--- main.h
+++ main.h
@@ -1,71 +1,50 @@
-#ifndef HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#define HOLBERTON_H
-
-
-
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
-#include <stddef.h>
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
 
-#include <stdlib.h>
+/* printf.c */
+int _printf(const char *, ...);
 
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
 
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+int buffer(char);
 
 /**
+ * struct _format - Typedef struct
  *
- *  * struct structprint - structure containing
- *
- *   * @q: the location and method to translate data to characters.
- *
- *    * @u: print function for specific type.
- *
- *     *
- *
- *      * Return: int
- *
- *       */
-
-typedef struct structprint
-
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
 {
-
-		char *q;
-
-			int (*u)(char *format, va_list);
-
-} structype;
+	char type;
+	int (*f)(va_list);
+} format;
 
 
-
-int _putchar(char ch);
-
-int _puts(char *string);
-
-int printc(char *format, va_list);
-
-int printstr(char *format, va_list);
-
-int (*driver(char *format))(char *format, va_list);
-
-int _printf(char *format, ...);
-
-int printint(char *format, va_list pa);
-
-int integer(int number);
-
-int contadordigit(int number);
-
-int _abs(int number);
-
-int printpercent(char *format, va_list pa);
-
-int printhex(char *format, va_list);
-
-int printHEX(char *format, va_list);
-
-int printocta(char *format, va_list);
-
-int print_unsign(char *format, va_list);
-
-#endif
+#endif /* MAIN_H */
