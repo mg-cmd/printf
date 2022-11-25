3b43fde35f9d03b670514db37b228af09714e468
mg-cmd <gmebrahtu8@gmail.com>
2022-11-25 10:22:22 -0800

    Task printf_37.c

---
+++ printf_37.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * printf_37 - prints the % sign (% is the ASCII code for 37).
+ *
+ *   * Return: 1.
+ *
+ *    */
+
+
+
+int printf_37(void)
+
+{
+
+	_putchar(37);
+
+	return (1);
+
+}
