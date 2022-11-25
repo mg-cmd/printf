3588345c81be0b5c8ffc62d1611735b37e97330c
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-25 10:59:18 -0800

    Update printf_srev.c

--- printf_srev.c
+++ printf_srev.c
@@ -1,49 +1,25 @@
 #include "main.h"
-
 #include <stdarg.h>
-
 #include <stdio.h>
 
 
-
-
-
 /**
+ * printf_srev - function that prints a string in reverse
+ * @args: type struct va_arg where is allocated printf arguments
  *
- *  * printf_srev - function that prints a string in reverse
- *
- *   * @args: type struct va_arg where is allocated printf arguments
- *
- *    *
- *
- *     * Return: the string
- *
- *      */
-
-
+ * Return: the string
+ */
 
 int printf_srev(va_list args)
-
 {
-
-	char *s = va_arg(args, char*);
-
-	int i;
-
-	int j = 0;
-
-	if (s == NULL)
-
-		s = "(null)";
-
-	while (s[j] != '\0')
-
-		j++;
-
-	for (i = j - 1; i >= 0; i--)
-
-		_putchar(s[i]);
-
-	return (j);
-
+char *s = va_arg(args, char*);
+int i;
+int j = 0;
+if (s == NULL)
+s = "(null)";
+while (s[j] != '\0')
+j++;
+for (i = j - 1; i >= 0; i--)
+_putchar(s[i]);
+return (j);
 }
