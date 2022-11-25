ca8f4ea90b476136bc6a266e127bd6785ae4fbd5
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-25 10:23:09 -0800

    Update printf_37.c

--- printf_37.c
+++ printf_37.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * printf_37 - prints the % sign (% is the ASCII code for 37).
- *
- *   * Return: 1.
- *
- *    */
-
-
+ * printf_37 - prints the % sign (% is the ASCII code for 37).
+ * Return: 1.
+ */
 
 int printf_37(void)
-
 {
-
-	_putchar(37);
-
-	return (1);
-
+_putchar(37);
+return (1);
 }
