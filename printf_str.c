191f067c1e0d8462c888f17af2229ab5e60a870e
mg-cmd <gmebrahtu8@gmail.com>
2022-11-25 11:00:26 -0800

    Task printf_str.c

---
+++ printf_str.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+
+
+/**
+ *
+ *  * _strlen - Returns the lenght of a string.
+ *
+ *   * @s: Type char pointer
+ *
+ *    * Return: c.
+ *
+ *     */
+
+
+
+int _strlen(char *s)
+
+{
+
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+
+		;
+
+	return (c);
+
+}
+
+
+
+/**
+ *
+ *  * _strlenc - Strlen function but applied for constant char pointer s
+ *
+ *   * @s: Type char pointer
+ *
+ *    * Return: c
+ *
+ *     */
+
+
+
+int _strlenc(const char *s)
+
+{
+
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+
+		;
+
+	return (c);
+
+}
