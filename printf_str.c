35c7c6c295ea8a7dfae7cd1893c8660bd491d610
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-25 11:01:04 -0800

    Update printf_str.c

--- printf_str.c
+++ printf_str.c
@@ -1,63 +1,32 @@
 #include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
 
-
-
-
 /**
- *
- *  * _strlen - Returns the lenght of a string.
- *
- *   * @s: Type char pointer
- *
- *    * Return: c.
- *
- *     */
-
-
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: c.
+ */
 
 int _strlen(char *s)
-
 {
-
-	int c;
-
-	for (c = 0; s[c] != 0; c++)
-
-		;
-
-	return (c);
-
+int c;
+for (c = 0; s[c] != 0; c++)
+;
+return (c);
 }
 
-
-
 /**
- *
- *  * _strlenc - Strlen function but applied for constant char pointer s
- *
- *   * @s: Type char pointer
- *
- *    * Return: c
- *
- *     */
-
-
+ * _strlenc - Strlen function but applied for constant char pointer s
+ * @s: Type char pointer
+ * Return: c
+ */
 
 int _strlenc(const char *s)
-
 {
-
-	int c;
-
-	for (c = 0; s[c] != 0; c++)
-
-		;
-
-	return (c);
-
+int c;
+for (c = 0; s[c] != 0; c++)
+;
+return (c);
 }
